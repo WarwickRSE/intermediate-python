03128fd4e46e0a5058ee49f6152ef225edc3669b
PipGrylls <pipgryllsastro@gmail.com>
2024-01-22 16:28:02 +0000

    datetime, need tests

--- python_scripts/classes_solution.py
+++ python_scripts/classes_solution.py
@@ -1,15 +1,24 @@
 # Like `def` for functions, `class` is a keyword for classes.
+from datetime import date
+
 class TestDog:  # Usually class names are capitalized.
     # The `__init__` method is the constructor.
     # It is called when an instance of the class is created.
     # The first argument is always `self`.
     # `self` refers to the instance of the class.
-    def __init__(self, name, owner, sex, age, color, tricks=None):
+    def __init__(self, name, owner, sex, dob, color, tricks=None):
         self.name = name
-        self.age = age
+        self._dob = date(dob)
         self.owner = owner
         self._sex = sex
         self._color = color
+        if type(dob) is date:
+            self._dob = dob
+        elif type(dob) is str:
+            self._dob = date.fromisoformat(dob)
+        else:
+            raise TypeError("Date of birth must be date or string.")
+            
         if tricks is None:
             # If no tricks supplied, set to empty set.
             self.tricks = set([])
@@ -23,6 +32,8 @@ class TestDog:  # Usually class names are capitalized.
             elif type(tricks) is set:
                 # If set of tricks supplied, set to set of tricks.
                 self._tricks = tricks
+            else:
+                raise TypeError("Tricks must be string, list, or set.")
 
     @property
     def sex(self):
@@ -31,6 +42,11 @@ class TestDog:  # Usually class names are capitalized.
     @property
     def color(self):
         return self._color
+
+    @property
+    def age(self):
+        # use datetime to get age in years
+        return (date.today() - self._dob).days // 365
 
     @property
     def tricks(self):
